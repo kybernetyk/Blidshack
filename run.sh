a56d533268e4a0e2ab936bb0bb6268ff8851ffea
jsz <jszpilewski@me.com>
2011-06-15 15:06:06 +0200

    no unicorns allowed

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gomake && ./getblids.sh && ./blids && ./metadata.py && mv aktkartepolengrau.jpg_metadata.txt pl_metadata.txt && mv aktkartegergrau.jpg_metadata.txt de_metadata.txt
+gomake && ./getblids.sh && ./blids && ./metadata.py && mv aktkartepolengrau.jpg_metadata.txt pl_metadata.txt && mv aktkartegergrau.jpg_metadata.txt de_metadata.txt && rm -f aktkarte*
