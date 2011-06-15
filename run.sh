71049bb3077d489282f3154658b115dcb21dc777
jsz <jszpilewski@me.com>
2011-06-15 15:05:05 +0200

    wuafagaa

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gomake && ./getblids.sh && ./blids && ./metadata.py
+gomake && ./getblids.sh && ./blids && ./metadata.py && mv aktkartepolengrau.jpg_metadata.txt pl_metadata.txt && mv aktkartegergrau.jpg_metadata.txt de_metadata.txt
